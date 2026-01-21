79be2cc06191b9876a39566f28f3821966c93147
jaimehworks-max <jaimehworks@gmail.com>
2026-01-21 14:21:15 -0500

    Subiendo reto de estado

---
+++ card.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./Producto.css";
+
+const ProductoCard = () => {
+  const [cantidad, setCantidad] = useState(0);
+
+  const aumentar = () => {
+    setCantidad(cantidad + 1);
+  };
+
+  const disminuir = () => {
+    if (cantidad > 0) {
+      setCantidad(cantidad - 1);
+    }
+  };
+
+  return (
+    <div className="card">
+      <img
+        src="https://m.media-amazon.com/images/I/61-PblYntsL._AC_SL1500_.jpg"
+        alt="Consola"
+      />
+
+      <h3>Consola de videojuegos</h3>
+
+      <p>
+        Esta es una consola de videojuegos que entretiene a los programadores en
+        sus breaks.
+      </p>
+
+      <div className="contador">
+        <button onClick={disminuir}>-</button>
+        <span>{cantidad}</span>
+        <button onClick={aumentar}>+</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductoCard;
